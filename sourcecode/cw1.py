ba469638f2765a322e03aaf06ce233c375c40195
Gregor Bell <gregorbell@virginmedia.com>
2018-10-14 21:57:45 +0100

    Created an array of sword data in cw1.py. Updated swords.html and added sword_details.html. Added a new route that creates a page for each sword generated from the array. These pages are located at http://set09103.napier.ac.uk:9112/swords/<sword_name>/ and have information about the specified sword and an image.

--- sourcecode/cw1.py
+++ sourcecode/cw1.py
@@ -1,17 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, abort, json, url_for
 app = Flask(__name__)
+
+swords = [
+		{
+		'name':'Blackfyre',
+		'material': 'Valyrian Steel',
+		'owner': 'House Targaryen',
+		'status': 'Missing'
+		},
+
+		{
+		'name': 'Brightroar',
+		'material': 'Valyrian Steel',
+		'owner': 'Tommen II Lannister',
+		'status': 'Missing'
+		},
+
+		{
+		'name': 'Dark Sister',
+		'material': 'Valyrian Steel',
+		'owner': 'Visenya Targaryen, Daemon Targaryen, Aemon Targaryen, Brynden Rivers',
+		'status': 'Missing'
+		},
+
+		{
+		'name': 'Dawn',
+		'material': '"Fallen Star" Metal',
+		'owner': '"The Sword of the Morning" (House Dayne)',
+		'status': 'Returned to Starfall by Eddard Stark'
+		},
+		
+		{
+		'name': 'Forrester Greatsword',
+		'material': 'Unknown',
+		'owner': 'House Forrester',
+		'status': 'Unknown (likely in the posession of House Whitehill)'		
+		}
+	]
 
 @app.route('/')
 def root():
 	return render_template('home.html'), 200
 
-@app.route('/swords/')
-def swords():
-	return render_template('swords.html')
-
 @app.route('/daggers/')
 def daggers():
 	return render_template('daggers.html')
+
+@app.route('/swords/', methods=['GET'])
+def swords_list():
+	return render_template('swords.html', swords=swords)
+
+@app.route('/swords/<sword_name>/')
+def sword_details(sword_name):
+	return render_template('sword_details.html', swords=swords, sword_name=sword_name)
 
 if __name__ == ("__main__"):
 	app.run(host='0.0.0.0', debug=True)
