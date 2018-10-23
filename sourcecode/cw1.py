0cbaea5694715406117b9820738fc78849a21178
Gregor Bell <gregorbell@virginmedia.com>
2018-10-23 03:14:25 +0100

    Updated home page. Added list of owners pages. Added delete form. Added redirects from upload and delete to swords. Updated sword details page (images now responsive). Created template for 404 errors. Made it so that the list of swords, materials and owners are alphabetised and dynamically update when a new sword is added to json or a sword is removed. Updated favicons.

--- sourcecode/cw1.py
+++ sourcecode/cw1.py
@@ -4,11 +4,6 @@ app = Flask(__name__)
 with open('static/data/swords.json') as swords_json:
 	swords = json.load(swords_json)
 
-with open('static/data/blades.json') as blades_json:
-	blades_loaded_json = json.load(blades_json)
-	blades = blades_loaded_json["swords"]
-	
-
 @app.route('/')
 def root():
 	return redirect(url_for('home'))
@@ -17,13 +12,10 @@ def root():
 def home():
 	return render_template('home.html'), 200
 
-@app.route('/test/', methods=['GET'])
-def test():
-	return jsonify(blades)
-
 @app.route('/swords/', methods=['GET'])
 def swords_list():
 	return render_template('swords.html', swords=swords)
+
 
 @app.route('/swords/<sword_name>/')
 def sword_details(sword_name):
@@ -38,21 +30,55 @@ def sword_details(sword_name):
 	else:
 		abort(404)
 
-material_names=[]
+@app.route('/owners/')
+def owners():
+	owner_names=[]
 
-for sword in swords:
-	material_names.append(sword["material"])
+	for sword in swords:
+		owner_names.append(sword["owner"])
 
-material_names_distinct= sorted(list(set(material_names)))
+	owner_names_distinct= sorted(list(set(owner_names)))
+	
+	return render_template('owners.html', swords=swords, owner_names_distinct=owner_names_distinct)
+
+@app.route('/owners/<owner_name>/')
+def owner_weapons(owner_name):
+	owner_names=[]
+
+	for sword in swords:
+		owner_names.append(sword["owner"])
+	
+	owner_names_distinct = sorted(list(set(owner_names)))
+
+	if owner_name in owner_names_distinct:
+		return render_template('owner_weapons.html', swords=swords, owner_names_distinct=owner_names_distinct, owner_name=owner_name)
+
+	else:
+		abort(404)
 
 @app.route('/materials/')
 def materials():		
+	material_names=[]
+	
+	for sword in swords:
+		material_names.append(sword["material"])
+	
+	material_names_distinct= sorted(list(set(material_names)))
+	
 	return render_template('materials.html', swords=swords, material_names_distinct=material_names_distinct)
 
 @app.route('/materials/<material_name>/')
 def material_weapons(material_name):
+	material_names=[]
+	
+	for sword in swords:
+		material_names.append(sword["material"])
+	
+	material_names_distinct= sorted(list(set(material_names)))
+
 	if material_name in material_names_distinct:
 		return render_template('material_weapons.html', swords=swords, material_names_distinct=material_names_distinct, material_name=material_name)
+	
 	else:
 		abort(404)
 
@@ -80,23 +106,42 @@ def upload():
 		with open('static/data/swords.json', 'w') as swords_json:
 			json.dump(swords, swords_json)
 		
-		return jsonify(swords)
+		return redirect(url_for('swords_list'))
 
 	else:
 		return render_template('upload.html')
 
-@app.route('/delete/<sword_name>/', methods=['DELETE','GET'])
-def delete(sword_name):
+@app.route('/delete/', methods=['POST','GET'])
+def delete():
+	if request.method == 'POST':
+		print request.form
+		name = request.form['name']
+		
+		for sword in swords:
+			if name == sword["name"]:
+				swords.remove(sword)
+		
+		with open('static/data/swords.json', 'w') as swords_json:
+			json.dump(swords, swords_json)
+
+		return redirect(url_for('swords_list'))
+
+	else:
+		return render_template('delete.html')
+
+@app.route('/delete/<sword_name>/', methods=['GET'])
+def delete_weapon(sword_name):
 	for sword in swords:
 		if sword_name == sword["name"]:
 			swords.remove(sword)
 	with open('static/data/swords.json', 'w') as swords_json:
 		json.dump(swords, swords_json)
-	return jsonify(swords)
+
+	return redirect(url_for('swords_list'))
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return "This page does not exist ya dingus", 404
+	return render_template('404.html'), 404
 
 if __name__ == ("__main__"):
 	app.run(host='0.0.0.0', debug=True)
